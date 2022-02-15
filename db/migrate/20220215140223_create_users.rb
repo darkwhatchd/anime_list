6d58a67e3e4c0649fdb2054931c9e261c4810a57
Daniel Evani <danielevani97@gmail.com>
2022-02-15 11:45:26 -0300

    Testes do model User, e model feitos

--- db/migrate/20220215140223_create_users.rb
+++ db/migrate/20220215140223_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.integer :profile
+      t.integer :profile, default: 1
 
       t.timestamps
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to define_enum_for(:profile).with_values({ admin: 0, otaku: 1 }) }
 end
